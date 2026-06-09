5a3b7826e41e2e05ca17e74bbeb4fa5da2e176e0
Lokesh Mahamulkar <lokeshmaha0903@gmail.com>
2026-06-09 23:40:27 +0530

    changed Username

--- ExaMinds/ExaMinds/settings.py
+++ ExaMinds/ExaMinds/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ExaMinds.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
